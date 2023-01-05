e22ae2606324a94251ae0283d473088454f86759
nbarnes <nick.a.barnes44@gmail.com>
2023-01-05 08:58:42 -0500

    update header file for adjacency list

--- include/adjacency_list.h
+++ include/adjacency_list.h
@@ -5,18 +5,42 @@
 #include <stdlib.h>
 
 #include "exit_codes.h"
-#include "linked_list.h"
+#include "linked_lists/singly_linked_list.h"
+
+typedef struct vertex
+{
+    size_t label;
+    singly_linked_list_t *neighbors;
+} vertex_t;
 
 typedef struct graph
 {
-    size_t num_vertices;
-    linked_list_t *adj_lists;
+    bool is_directed;
+    array_list_t *vertices;
 } graph_t;
 
-graph_t *graph_create(size_t vertices);
+graph_t *adj_list_create(size_t vertices);
 
-exit_code_t add_edge(graph_t *graph, void *src, void *dest);
+exit_code_t add_vertex(graph_t *graph, void *vertex);
 
-void graph_print(graph_t *graph);
+exit_code_t adj_list_add_edge(graph_t *graph, void *src, void *dst);
+
+exit_code_t adj_list_remove_vertex(graph_t *graph, void **vertex);
+
+exit_code_t adj_list_remove_edge(graph_t *graph, void *src, void **dst);
+
+exit_code_t adj_list_get_list(graph_t *graph, void *vertex, singly_linked_list_t **list);
+
+int adj_list_is_adjacent(graph_t *graph, void *src, void *dest);
+
+singly_linked_list_t adj_list_get_lists(graph_t *graph);
+
+int adj_list_vertex_count(graph_t *graph);
+
+int adj_list_edge_count(graph_t *graph);
+
+void adj_list_destroy();
+
+void adj_list_print(graph_t *graph);
 
 #endif
